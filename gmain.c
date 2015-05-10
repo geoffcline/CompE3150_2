6e6fce8ca72808ccaf247d64e0e224cf3d9a5138
Geoffrey Cline <geoffreyc@outlook.com>
2015-05-10 00:57:09 -0500

    fix spacing in main

--- gmain.c
+++ gmain.c
@@ -10,39 +10,41 @@ void main ( void )
 	while(~bt9);
 	
 	while(1)
-  {
-    while(~bt1)
+  	{
+    	while(~bt1)
 		{
 			send_test();
 			Song_Name(1);
-			runsong(1);
-			
-		} 
-    while(~bt2)
+			runsong(1);	
+		}
+
+    	while(~bt2)
 		{
 			Song_Name(0);
 			runsong(0);
 		}
-    while(~bt3)
+    	
+    	while(~bt3)
 		{
-      piano();
+      		piano();
 		}
+		
 		while(~bt4)
 		{
-      wind_up_song();
+      		wind_up_song();
 		}
   }	
 }
 
 void Delay1(signed int i)
 {
-  TH1=i >> 8;
-  TL1=i;
-  TR1=1;
+	TH1=i >> 8;
+  	TL1=i;
+  	TR1=1;
 	TF1=0;
-  while(!TF1);
-  TR1=0;
-  TF1=0;
+  	while(!TF1);
+  	TR1=0;
+  	TF1=0;
 	
 	return;
 }	
